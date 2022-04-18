2015b0f4335cc881b0e470114c483c2ce6927c5f
unknown <abdullahalomar048@gmail.com>
2022-04-18 14:28:25 +0600

    implement google sign in authentication system

--- src/Pages/Login/SocialSignin/SocialSignin.js
+++ src/Pages/Login/SocialSignin/SocialSignin.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { Navigate, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import google from '../../../images/icons8-google.svg';
 
 const SocialSignin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorElement;
+
+    if (error) {
+        errorElement = <div>
+            <h5 className='text-danger'>Error: {error.message}</h5>
+          </div>
+        
+    }
+    
+    if (user) {
+        navigate('/home')
+    }
+
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -9,8 +27,9 @@ const SocialSignin = () => {
             <p className='mt-2 px-3'>or</p>
             <div style={{ height: '1px' }} className='bg-secondary w-50'></div>
             </div>
+            {errorElement}
             <div>
-                <button className='btn btn-info w-100'><img src={google} width={34} alt="" /> Google Sign In</button>
+                <button onClick={()=>signInWithGoogle()} className='btn btn-info w-100'><img src={google} width={34} alt="" /> Google Sign In</button>
             </div>
         </div>
     );
